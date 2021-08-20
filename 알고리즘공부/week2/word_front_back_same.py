5eae0ca57bf5d4ba1803a6fbebdd140385c4f8c9
Hyun - Woo <nikel4610@gmail.com>
2021-08-20 11:36:44 +0900

    Update word_front_back_same.py

--- 알고리즘공부/week2/word_front_back_same.py
+++ 알고리즘공부/week2/word_front_back_same.py
@@ -2,12 +2,19 @@ input = "abcba"
 
 
 def is_palindrome(string):
-    n = len(string)
-    for i in range(n):
-        if string[i] != string[n -1 -i]:
-            return False
-        
-    return True
+    #n = len(string)
+    #for i in range(n):
+    #    if string[i] != string[n -1 -i]:
+    #        return False
+
+    #return True
+    if len(string) <= 1:
+        return True
+    
+    if string[0] != string[-1]:
+        return False
+
+    return is_palindrome(string[1:-1])
 
 
 print(is_palindrome(input))
