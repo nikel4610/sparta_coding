4d18a8783941a56a8bbcef4282cda17da82d96a0
Hyun - Woo <nikel4610@gmail.com>
2021-08-27 15:21:21 +0900

    Update merge.py

--- 알고리즘공부/week3/merge.py
+++ 알고리즘공부/week3/merge.py
@@ -1,31 +1,38 @@
-array_a = [1, 2, 3, 5]
-array_b = [4, 6, 7, 8]
+array = [5, 3, 2, 1, 6, 8, 7, 4]
+
+
+def merge_sort(array):
+    if len(array) <= 1:
+        return array #탈출조건
+    mid = (len(array)) // 2
+    left_array = merge_sort(array[:mid])
+    right_array = merge_sort(array[mid:])
+    return merge(left_array, right_array)
 
 
 def merge(array1, array2):
-    # 이 부분을 채워보세요!
-    array_c = []
+    result = []
     array1_index = 0
     array2_index = 0
     while array1_index < len(array1) and array2_index < len(array2):
         if array1[array1_index] < array2[array2_index]:
-            array_c.append(array1[array1_index])
+            result.append(array1[array1_index])
             array1_index += 1
         else:
-            array_c.append(array2[array2_index])
+            result.append(array2[array2_index])
             array2_index += 1
 
     if array1_index == len(array1):
         while array2_index < len(array2):
-            array_c.append(array2[array2_index])
-            array2_index +=1
-            
+            result.append(array2[array2_index])
+            array2_index += 1
+
     if array2_index == len(array2):
         while array1_index < len(array1):
-            array_c.append(array1[array1_index])
-            array1_index +=1
+            result.append(array1[array1_index])
+            array1_index += 1
 
-    return array_c
+    return result
 
 
-print(merge(array_a, array_b))  # [1, 2, 3, 4, 5, 6, 7, 8] 가 되어야 합니다!
+print(merge_sort(array))  # [1, 2, 3, 4, 5, 6, 7, 8] 가 되어야 합니다!
