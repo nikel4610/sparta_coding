fd40791da5bf916881dd1b6a68d04c3c95084c47
Hyun - Woo <nikel4610@gmail.com>
2021-09-01 16:03:47 +0900

    Create dfs_stack.py

---
+++ 알고리즘공부/week4/dfs_stack.py
@@ -0,0 +1,30 @@
+graph = {
+    1: [2, 5, 9],
+    2: [1, 3],
+    3: [2, 4],
+    4: [3],
+    5: [1, 6, 8],
+    6: [5, 7],
+    7: [6],
+    8: [5],
+    9: [1, 10],
+    10: [9]
+}
+
+
+def dfs_stack(adjacent_graph, start_node):
+    # 구현해보세요!
+    stack = [start_node]
+    visited = []
+
+    while stack:
+        current_node = stack.pop()
+        visited.append(current_node) # [1]
+        for adjacent_node in adjacent_graph[current_node]:
+            if adjacent_node not in visited:
+                stack.append(adjacent_node)
+    return visited
+
+
+print(dfs_stack(graph, 1))  # 1 이 시작노드입니다!
+# [1, 9, 10, 5, 8, 6, 7, 2, 3, 4] 이 출력되어야 합니다!
