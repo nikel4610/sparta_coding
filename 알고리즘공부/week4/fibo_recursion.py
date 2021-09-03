a8d026baeb38809f7836ca742099e395f21c7996
Hyun - Woo <nikel4610@gmail.com>
2021-09-03 14:37:10 +0900

    Update fibo_recursion.py

--- 알고리즘공부/week4/fibo_recursion.py
+++ 알고리즘공부/week4/fibo_recursion.py
@@ -1,11 +1,30 @@
-input = 20
+#input = 20 숫자가 크면 느림
 
 
-def fibo_recursion(n):
+#def fibo_recursion(n):
     # 구현해보세요!
-    if n == 1 or n == 2: #fibo_recursion(3) = fibo_recursion(2) + fibo_recursion(1)
-        return 1
-    return fibo_recursion(n - 1) + fibo_recursion(n - 2)
+    #if n == 1 or n == 2: #fibo_recursion(3) = fibo_recursion(2) + fibo_recursion(1)
+    #   return 1
+    #return fibo_recursion(n - 1) + fibo_recursion(n - 2)
 
 
-print(fibo_recursion(input))  # 6765
+#print(fibo_recursion(input))  # 6765
+
+input = 50 #숫자가 커도 빠름
+
+# memo 라는 변수에 Fibo(1)과 Fibo(2) 값을 저장해놨습니다!
+memo = {
+    1: 1,
+    2: 1
+}
+
+def fibo_dynamic_programming(n, fibo_memo):
+    # 구현해보세요!
+    if n in fibo_memo:
+        return fibo_memo[n]
+
+    nth_fibo = fibo_dynamic_programming(n-1,fibo_memo) + fibo_dynamic_programming(n-2, fibo_memo)
+    fibo_memo[n] = nth_fibo
+    return nth_fibo
+
+print(fibo_dynamic_programming(input, memo))
